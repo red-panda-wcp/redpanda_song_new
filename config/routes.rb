f1113e8b97e4bb90f3e81eddd9f1611e7e31325f
shkobm <scrt.8shbabe@gmail.com>
2018-01-19 17:44:59 +0900

    create login,logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index' # devise実装時の仮設定部分。マージするとき再考が必要
+
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
